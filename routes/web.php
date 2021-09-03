dd74d12ed68876361a971d40ea2e8818ec39754e
taku matsunaga <skyline199306@yahoo.co.jp>
2021-09-03 23:07:24 +0900

    add

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@
 
 Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index'); //-- この行を編集
-Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth'); //-- この行を変更
-
+Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth'); //-- この行を変更
+Route::resource('/articles', 'ArticleController')->only(['show']);
