83181065f2705b4e6054ccbce3142db2a266b5d1
taku matsunaga <skyline199306@yahoo.co.jp>
2021-10-06 20:58:57 +0900

    add

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -22,12 +22,12 @@
     </div>
     <div class="card-body">
       <div class="card-text">
-        <a href="" class="text-muted">
-          {{ $user->count_followings }} フォロー
-        </a>
-        <a href="" class="text-muted">
-          {{ $user->count_followers }} フォロワー
-        </a>
+        <a href="{{ route('users.followings', ['name' => $user->name]) }}" class="text-muted">
+            {{ $user->count_followings }} フォロー
+          </a>
+          <a href="{{ route('users.followers', ['name' => $user->name]) }}" class="text-muted">
+            {{ $user->count_followers }} フォロワー
+          </a>
       </div>
     </div>
   </div>
