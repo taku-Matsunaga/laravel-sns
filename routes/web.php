04aa7fb86560e7244c4348f0a33bac43f92f423d
taku matsunaga <skyline199306@yahoo.co.jp>
2021-10-08 21:07:51 +0900

    add

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 Route::prefix('login')->name('login.')->group(function () {
     Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+    Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('{provider}.callback');
 });
 Route::get('/', 'ArticleController@index')->name('articles.index'); //-- この行を編集
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth'); //-- この行を変更
